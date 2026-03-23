de8fdf1cb8ae486165759bcbd6d70c572e4646a1
Giovanni <giovanni.charles@gmail.com>
2026-03-23 12:02:50 +0000

    Bump up sample batch size

--- scripts/run_hierarchical_benchmark.py
+++ scripts/run_hierarchical_benchmark.py
@@ -130,7 +130,7 @@ def run_benchmark(
     )
     start_time = time.time()
 
-    algorithm_kwargs['sample_batch_size'] = 1_000
+    algorithm_kwargs['sample_batch_size'] = 10_000
 
     samples, actual_num_sims, log_prob_true, posterior = run_algorithm(
         task=task,
